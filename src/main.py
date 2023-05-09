e7e8a336af7d77ca825cf746e11c738bf38cdf47
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-05-09 19:58:27 +0900

    ✨ feat: add get all posts api

--- src/main.py
+++ src/main.py
@@ -1,15 +1,43 @@
-from typing import Union
-from fastapi import FastAPI
-from config import Settings
-import os
-from dotenv import load_dotenv
+from typing import Union, Dict
+from fastapi import FastAPI, Depends
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# load_dotenv(os.path.join(BASE_DIR, ".env"))
-app = FastAPI()
-settings = Settings()
+import db_model.database
+import db_model.models
+import db_model.crud
+
+db_model.models.Base.metadata.create_all(bind = db_model.database.engine)
+
+app = FastAPI(
+    title = "E-DEVELOPERS SERVER",
+    description= "for edev website",
+    version = "1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+def get_db():
+    db = db_model.database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 async def root():
-    env = settings.SQLALCHEMY_DATABASE_URL
-    return {"result": env}
+    return "Hello! This is devewha's API Server";
+
+@app.get("/posts")
+async def findPosts(db: Session = Depends(get_db)):
+    postResponseDTOList = []
+    posts = db_model.crud.get_all_posts(db=db)
+    return posts
