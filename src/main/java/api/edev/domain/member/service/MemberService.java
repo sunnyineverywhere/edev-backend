90299749e56e67b761e946bed096612116e2ef54
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-06-06 01:49:01 +0900

    ✨ feat: member service 트랜잭션

--- src/main/java/api/edev/domain/member/service/MemberService.java
+++ src/main/java/api/edev/domain/member/service/MemberService.java
@@ -5,19 +5,14 @@ import api.edev.domain.member.storage.Member;
 import api.edev.domain.member.storage.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    public void addTestMember() {
-        Member member = Member.builder()
-                .email("abc@gmail.com")
-                .name("abc")
-                .profile("profile").build();
-        memberRepository.save(member);
-    }
 
     public void modifyMemberInfo(Member member, MemberInfoModifyReq request) {
         member.modifyMemberInfo(request.getName());
