5fc52818f803fea20f89f61a11104d84921331f7
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-06-06 16:36:31 +0900

    ✨ feat: Cors Configuration 추가

--- src/main/java/api/edev/global/config/WebConfig.java
+++ src/main/java/api/edev/global/config/WebConfig.java
@@ -3,7 +3,9 @@ package api.edev.global.config;
 import api.edev.domain.member.storage.AuthMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +18,20 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(authMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedOrigins("https://edev-sunnyineverywhere.vercel.app/")
+                .allowedOrigins("")
+                .allowedMethods(HttpMethod.GET.name())
+                .allowedMethods(HttpMethod.POST.name())
+                .allowedMethods(HttpMethod.PUT.name())
+                .allowedMethods(HttpMethod.DELETE.name())
+                .allowedHeaders("Authorization")
+                .allowedHeaders("refresh-token")
+                .allowCredentials(true);
     }
 }
